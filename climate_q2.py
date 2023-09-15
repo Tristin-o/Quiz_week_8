245c3ee52e43919a5d8cd41f16c87882584084e7
Tristin Organ <tristin.organ@griffithuni.edu.au>
2023-09-15 17:53:31 +1000

    used pandas to read the csv file, appended the lists using the data['list name']

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,26 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+data = pd.read_csv(r".\climate.csv")
+
 years = []
 co2 = []
 temp = []
 
+years.append(data['Year'])
+co2.append(data['CO2'])
+temp.append(data['Temperature'])
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
+plt.plot(years, co2, 'r*-')
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
 plt.xlabel("Year (decade)") 
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
+plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.savefig("co2_temp_2.png")
 
